109af02a30c0fb19dace0d8845c7e739bdc69a82
Zayne <star1582@naver.com>
2022-12-24 23:55:50 +0900

    json-server db 생성

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import { useState } from "react";
+import axios from "axios";
 
 function App() {
   const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const onSubmit = () => {
+    axios.post("http://localhost:3001/posts", {
+      title,
+      body,
+    });
+  };
   return (
     <div className="container">
       <div className="mb-3">
@@ -14,7 +22,20 @@ function App() {
           }}
         />
       </div>
-      <button className="btn btn-primary">Post</button>
+      <div className="mb-3">
+        <label className="form-label">Body</label>
+        <textarea
+          className="form-control"
+          value={body}
+          onChange={(event) => {
+            setBody(event.target.value);
+          }}
+          rows="20"
+        />
+      </div>
+      <button className="btn btn-primary" onClick={onSubmit}>
+        Post
+      </button>
     </div>
   );
 }
