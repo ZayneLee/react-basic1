dd7dd11be59e6bafb9ddd6881e583cb46b8925fc
Zayne <star1582@naver.com>
2023-01-11 17:48:14 +0900

    Pagination 8

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,14 +1,26 @@
 import propTypes from "prop-types";
 
-const Pagination = ({ currentPage, numberOfPages, onClick }) => {
+const Pagination = ({ currentPage, numberOfPages, onClick, limit }) => {
+  const currentSet = Math.ceil(currentPage / limit);
+  const lastSet = Math.ceil(numberOfPages / limit);
+  const startPage = limit * (currentSet - 1) + 1;
+  const numberOfPageForSet =
+    currentSet === lastSet ? numberOfPages % limit : limit;
   return (
     <nav aria-label="Page navigation example">
       <ul className="pagination justify-content-center">
-        <li className="page-item disabled">
-          <a className="page-link">Previous</a>
-        </li>
-        {Array(numberOfPages)
-          .fill(1)
+        {currentSet !== 1 && (
+          <li className="page-item">
+            <div
+              className="page-link cursor-pointer"
+              onClick={() => onClick(startPage - limit)}
+            >
+              Previous
+            </div>
+          </li>
+        )}
+        {Array(numberOfPageForSet)
+          .fill(startPage)
           .map((value, index) => value + index)
           .map((pageNumber) => {
             return (
@@ -30,11 +42,16 @@ const Pagination = ({ currentPage, numberOfPages, onClick }) => {
             );
           })}
 
-        <li className="page-item">
-          <a className="page-link" href="#">
-            Next
-          </a>
-        </li>
+        {currentSet !== lastSet && (
+          <li className="page-item">
+            <div
+              className="page-link cursor-pointer"
+              onClick={() => onClick(startPage + limit)}
+            >
+              Next
+            </div>
+          </li>
+        )}
       </ul>
     </nav>
   );
@@ -44,10 +61,12 @@ Pagination.propTypes = {
   currentPage: propTypes.number,
   numberOfPages: propTypes.number.isRequired,
   onClick: propTypes.func.isRequired,
+  limit: propTypes.number,
 };
 
 Pagination.defaultProps = {
   currentPage: 1,
+  limit: 5,
 };
 
 export default Pagination;
